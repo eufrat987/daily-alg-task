341396c0b47161998dce0073c4c045f65528a5b9
Łukasz Żywański <lukasz.zywanski@sollers.eu>
2021-12-08 19:04:30 +0100

    h-Index

---
+++ 12.21/h-Index.py
@@ -0,0 +1,30 @@
+# Hi, here's your problem today. This problem was recently asked by Amazon:
+
+# The h-index is a metric that attempts to measure the productivity 
+# and citation impact of the publication of a scholar. 
+# The definition of the h-index is if a scholar has at least h of their 
+# papers cited h times.
+
+# Given a list of publications of the number of citations a scholar has, 
+# find their h-index.
+
+# Example:
+# Input: [3, 5, 0, 1, 3]
+# Output: 3
+# Explanation:
+# There are 3 publications with 3 or more citations, 
+# hence the h-index is 3.
+
+# Here's a starting point:
+
+def hIndex(publications):
+    publications.sort()
+    l = len(publications)
+    for i in range(l-1,-1,-1):
+        c = l-i
+        if c >= publications[i]: 
+            return publications[i]
+
+
+print (hIndex([5, 3, 3, 1, 0]))
+# 3
